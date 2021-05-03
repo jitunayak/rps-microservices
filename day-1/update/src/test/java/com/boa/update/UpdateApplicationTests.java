4401a4280f27cbe4cd33bd67698dd79676eeea08
Jitu Nayak <jitunayak715@gmail.com>
2021-05-03 20:52:22 +0530

    first commit

---
+++ day-1/update/src/test/java/com/boa/update/UpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boa.update;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
